846ab9f905f4282eceb872b6ee2a12c8ad8a34af
dvrubio <dannarubio2198@gmail.com>
2024-03-14 14:01:02 -0500

    fix: messages

--- TravelConnectWeb/src/app/components/dashboard/dashboard.component.ts
+++ TravelConnectWeb/src/app/components/dashboard/dashboard.component.ts
@@ -1,97 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { JourneyService } from '../../services/journey/journey.service';
-import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {  FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Journey } from '../../models/journey,model';
 import { JourneyComponent } from "../journey/journey.component";
 @Component({
-    selector: 'app-dashboard',
-    standalone: true,
-    templateUrl: './dashboard.component.html',
-    styleUrl: './dashboard.component.css',
-    imports: [ReactiveFormsModule, JourneyComponent]
+  selector: 'app-dashboard',
+  standalone: true,
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+  imports: [ReactiveFormsModule, JourneyComponent]
 })
 export class DashboardComponent {
-  journey: Journey = new Journey;
-  origin: string = '';
-  destination:string = '';
+  @ViewChild('errorModal') modalError!: ElementRef;
+
+  journey: Journey = new Journey();
   formSearch: FormGroup;
-  buttonPressed: boolean = false;
   isLoading: boolean = false;
-  messageErrorOrigin:string = "";
-  messageErrorDestination:string = "";
-  constructor(private journeyService: JourneyService,private formBuilder: FormBuilder) {
+  messageErrors: { origin: string, destination: string } = { origin: '', destination: '' };
+
+  constructor(private journeyService: JourneyService, private formBuilder: FormBuilder) {
     this.formSearch = this.formBuilder.group({
       origin: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]],
       destination: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]]
-    }, { validator: this.originDestinationValidator });
+    });
   }
 
-  originDestinationValidator(control: AbstractControl) {
-    const origin = control.get('origin');
-    const destination = control.get('destination');
-
-    if (origin && destination && origin.value == destination.value) {
-      return { originDestinationValidator: true };
-    } else {
-
-      return null;
-    }
-  }
-  onInputChange(): void {
-    if (this.buttonPressed){
-      this.errosInput();
-
-    }
-  }
   searchJourney() {
-    this.buttonPressed = true;
     if (this.formSearch.valid) {
       this.isLoading = true;
-      this.journeyService.getData(this.formSearch.get("origin")?.value, this.formSearch.get("destination")?.value).subscribe(
+      const origin = this.formSearch.get("origin")?.value.toUpperCase();
+      const destination = this.formSearch.get("destination")?.value.toUpperCase();
+      this.journeyService.getData(origin, destination).subscribe(
         (data) => {
-        this.journey = data;
-        this.isLoading = false;
-      }, (error) => {
-        this.isLoading = false;
-      
-      });
-    }else{
-      this.errosInput();
+          this.journey = data;
+          this.isLoading = false;
+        }, 
+        (error) => {
+          this.isLoading = false;
+          this.openModal();
+        }
+      );
+    } else {
+      this.validateInput();
     }
   }
 
-  
-  errosInput(){
-    this.messageErrorDestination="";
-    this.messageErrorOrigin="";
-    if (this.origin.length<3 && this.origin.length!=0){
-      this.messageErrorOrigin+="The origin must have exactly 3 letters  ";
-    }
-    if (this.origin.length==0){
-      this.messageErrorOrigin+="Please enter an origin  ";
-    }
-    if (this.destination.length<3 && this.destination.length!=0){
-      this.messageErrorDestination+="The destination must have exactly 3 letters  ";
-    }
-    if (this.destination.length==0){
-      this.messageErrorDestination+="Please enter an destination  ";
-    }
-    if (this.destination==this.origin && this.destination.length!=0){
-      this.messageErrorDestination+="The destination must be different from the origin  ";
-    }
-    if (/[0-9\W]/.test(this.destination) ){
-      this.messageErrorDestination+="Only letters  ";
-    }
-    if (/[0-9\W]/.test(this.origin)){
-      this.messageErrorOrigin+="Only letters  ";
-    }
+  validateInput() {
+    this.formSearch.setValue({
+      origin: this.formSearch.get("origin")?.value.toUpperCase(),
+      destination: this.formSearch.get("destination")?.value.toUpperCase()
+    });
+    const origin = this.formSearch.get("origin")?.value;
+    const destination = this.formSearch.get("destination")?.value;
+    this.messageErrors.origin = this.getErrorMessage(origin, 'origin');
+    this.messageErrors.destination = this.getErrorMessage(destination, 'destination');
   }
-  containNumbers(inputString: string): boolean {
-    for (const char of inputString) {
-      if (/\d/.test(char)) {
-        return true;
-      }
-    }
-    return false;
+
+  getErrorMessage(value: string, field: string) {
+    if (!value) return "Please enter a " + field;
+    if (value.length < 3) return "The " + field + " must have exactly 3 letters";
+    if (/[0-9\W]/.test(value)) return "Only letters are allowed";
+    return "";
+  }
+
+  openModal() {
+    this.modalError.nativeElement.classList.add('show');
+  }
+
+  closeModal() {
+    this.modalError.nativeElement.classList.remove('show');
   }
 }
--- TravelConnectWeb/src/app/services/journey/journey.service.ts
+++ TravelConnectWeb/src/app/services/journey/journey.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, map, of, switchMap, tap, } from 'rxjs';
-import { ApiState } from '../../store/reducersRedux/reducers';
-import { Store } from '@ngrx/store';
+import { Observable, map, of, switchMap, tap, } from 'rxjs';
+import { DataState, RequestEntry } from '../../store/reducersRedux/reducers';
+import { Store, select } from '@ngrx/store';
 import * as actions from "../../store/actionsRedux/actions";
 
 @Injectable({
@@ -10,35 +10,35 @@ import * as actions from "../../store/actionsRedux/actions";
 })
 export class JourneyService {
 
-  constructor(private httpClient: HttpClient, private store: Store<ApiState>) {}
+  constructor(private httpClient: HttpClient, private store: Store<{ data: DataState }>) { }
 
-  getData(origin : string, destination: string): Observable<any> {
-    return this.httpClient.get(`/api/getJourney/${origin}/${destination}`);
+  // getData(origin : string, destination: string): Observable<any> {
+  //   return this.httpClient.get(`/api/getJourney/${origin}/${destination}`);
+  // }
+
+  getRequestEntryByOriginAndDestination(origin: string, destination: string): Observable<RequestEntry | undefined> {
+    return this.store.pipe(
+      select('data'),
+      map((dataState: DataState) => {
+        const entry = dataState.history.find(entry => entry.origin === origin && entry.destination === destination);
+        return entry;
+      })
+    );
   }
 
-  // getData(origin: string, destination: string): Observable<any> {
-  //   const key = `${origin}_${destination}`;
-  //   const cachedData = this.store.select(state => state.history);
-  //   return this.store.select(state => state.history).pipe(
-  //     switchMap(history => {
-  //       if (!history) {
-  //         return of(null);
-  //       }
-  //       const cachedData = history[key];
-  //       if (cachedData) {
-  //         return of(cachedData);
-  //       } else {
-  //         return this.httpClient.get(`/api/getJourney/${origin}/${destination}`).pipe(
-  //           tap(response => {
-  //             this.store.dispatch(actions.fetchSuccess({ origin, destination, data: response }));
-  //           }),
-  //           catchError(error => {
-  //             this.store.dispatch(actions.fetchFailure({ error }));
-  //             return of(null);
-  //           })
-  //         );
-  //       }
-  //     })
-  //   );
-  // }
+  getData(origin: string, destination: string): Observable<any> {
+    return this.getRequestEntryByOriginAndDestination(origin, destination).pipe(
+      switchMap((entry) => {
+        if (entry) {
+          return of(entry.data);
+        } else {
+          return this.httpClient.get(`/api/getJourney/${origin}/${destination}`).pipe(
+            tap(response => {
+              this.store.dispatch(actions.fetchSuccess({ origin, destination, data: response }));
+            })
+          );
+        }
+      })
+    );
+  }
 }
